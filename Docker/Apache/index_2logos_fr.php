7a60e89705b0cd4e43d14f900e97f3cfd4b273fc
YvosOnTheHub <yves.weisser@gmail.com>
2018-11-30 11:21:23 +0000

    Create index_2logos_fr.php

---
+++ Docker/Apache/index_2logos_fr.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<style>
+.background1 { background-image: url('netapp_io.jpg');background-repeat:no-repeat;background-position:left top;background-size:150px}
+.background2 { background-image: url('netapp_io.jpg');background-repeat:no-repeat;background-position:right top;background-size:150px}
+</style>
+<html><body>
+<div class="background1"><div class="background2">
+<center>
+<br><br><br><br><br><br>
+<h1>NetApp Demo</h1>
+<h1>Cette ligne sera la m&#234;me peu importe le container!</h1>
+<h1>mais celle-ci changera suivant le container : Container ID = <font color=#00ccff><?php echo gethostname(); ?></font></h1>
+<br><br><br><br><br><br>
+</center>
+</div></div>
+</body></html>
