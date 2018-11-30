df5541ba06e7c4ace847e59c276db4816430a43b
YvosOnTheHub <yves.weisser@gmail.com>
2018-11-30 11:22:53 +0000

    Create index_2logos_en.php

---
+++ Docker/Apache/index_2logos_en.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<style>
+.background1 { background-image: url('netapp_io.jpg');background-repeat:no-repeat;background-position:left top;background-size:150px}
+.background2 { background-image: url('netapp_io.jpg');background-repeat:no-repeat;background-position:right top;background-size:150px}
+</style>
+<html><body>
+<div class="background1"><div class="background2">
+<center>
+<br><br><br><br><br><br>
+<h1>NetApp Demo</h1>
+<h1>This line will never change!</h1>
+<h1>but this one is dynamic : Container ID = <font color=#00ccff><?php echo gethostname(); ?></font></h1>
+<br><br><br><br><br><br>
+</center>
+</div></div>
+</body></html>
