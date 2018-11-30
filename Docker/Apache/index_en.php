905884b35ef810778d8ecce5349ec459aac4ecd1
YvosOnTheHub <yves.weisser@gmail.com>
2018-11-30 11:24:08 +0000

    Create index_en.php

---
+++ Docker/Apache/index_en.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<style>
+.background1 { background-image: url('netapp_io.jpg');background-repeat:no-repeat;background-position:left top;background-size:150px}
+</style>
+<html><body>
+<div class="background1">
+<center>
+<br><br><br><br><br><br>
+<h1>NetApp Demo</h1>
+<h1>This line will never change!</h1>
+<h1>but this one is dynamic : Container ID = <font color=#00ccff><?php echo gethostname(); ?></font></h1>
+<br><br><br><br><br><br>
+</center>
+</div>
+</body></html>
