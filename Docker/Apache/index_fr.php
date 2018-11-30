ebc67d79c0e3b543b943a94c5a7a78ce562d1f7f
YvosOnTheHub <yves.weisser@gmail.com>
2018-11-30 11:20:29 +0000

    Create index_fr.php

---
+++ Docker/Apache/index_fr.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<style>.background { background-image: url('netapp_io.jpg');background-repeat:no-repeat;background-position:left top;background-size:150px}</style>
+<html><body><div class="background">
+<center>
+<br><br><br><br><br><br>
+<h1>NetApp Demo</h1>
+<h1>Cette ligne sera la m&#234;me peu importe le container!</h1>
+<h1>mais celle-ci changera suivant le container : Container ID = <font color=#00ccff><?php echo gethostname(); ?></font></h1>
+<br><br><br><br><br><br>
+</center>
+</div>
+</body></html>
