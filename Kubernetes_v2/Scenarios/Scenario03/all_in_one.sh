79ee18bb8806382d927796c325ab949693db4311
YvosLenovo <yweisser@netapp.com>
2020-09-10 18:00:04 +0200

    improved the sheel the all in one script

--- Kubernetes_v2/Scenarios/Scenario03/all_in_one.sh
+++ Kubernetes_v2/Scenarios/Scenario03/all_in_one.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+if [[ $(kubectl get ns | grep metallb-system | wc -l) = 1 ]]
+then
+   echo "#######################################################################################################"
+   echo "Install MetalLB"
+   echo "#######################################################################################################"
+
+   kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+   kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.3/manifests/metallb.yaml
+   kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+   echo "#######################################################################################################"
+   echo "Configure MetalLB"
+   echo "#######################################################################################################"
+
+   cd /root/LabNetApp/Kubernetes_v2/Addendum/Addenda07
+   kubectl apply -f metallb-configmap.yml
+fi
+
+
 echo "#######################################################################################################"
 echo "Install Helm"
 echo "#######################################################################################################"
