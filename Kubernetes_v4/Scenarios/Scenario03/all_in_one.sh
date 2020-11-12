d913d2ee506c03159036bf9501e245235ab31282
YvosLenovo <yweisser@netapp.com>
2020-11-12 13:57:14 +0100

    dashboard fix

--- Kubernetes_v4/Scenarios/Scenario03/all_in_one.sh
+++ Kubernetes_v4/Scenarios/Scenario03/all_in_one.sh
@@ -16,20 +16,20 @@ echo "Upgrade Prometheus Operator with Helm"
 echo "#######################################################################################################"
 
 helm upgrade prom-operator stable/prometheus-operator --namespace monitoring --set prometheusOperator.createCustomResource=false,grafana.persistence.enabled=true
+sleep 15s
 
 echo "#######################################################################################################"
 echo "Install Pie Chart Plugin in Grafana"
 echo "#######################################################################################################"
 
-sleep 15s
 kubectl exec -n monitoring -it $(kubectl get -n monitoring pod -l app.kubernetes.io/name=grafana --output=name) -c grafana -- grafana-cli plugins install grafana-piechart-panel
 kubectl scale -n monitoring deploy prom-operator-grafana --replicas=0
 kubectl scale -n monitoring deploy prom-operator-grafana --replicas=1
+sleep 15s
 
 echo "#######################################################################################################"
 echo "Create ConfigMap for Dashboards"
 echo "#######################################################################################################"
 
-sleep 15s
 kubectl create configmap -n monitoring cm-trident-dashboard --from-file=3_Grafana/Dashboards/
 kubectl label configmap -n monitoring cm-trident-dashboard grafana_dashboard=1
