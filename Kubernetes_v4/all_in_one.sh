bfc413df3c16e5a966e45da4bd723e89884974aa
Yvos <yves.weisser@gmail.com>
2021-01-22 15:47:01 +0100

    docker hub rate checks

--- Kubernetes_v4/all_in_one.sh
+++ Kubernetes_v4/all_in_one.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
+# OPTIONAL PARAMETERS: 
 # PARAMETER1: Docker hub login
 # PARAMETER2: Docker hub password
-
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied"
-    echo "Please add the following parameters to the shell script:"
-    echo " - Parameter1: Docker hub login"
-    echo " - Parameter2: Docker hub password"
-    exit 0
-fi
 
 echo
 echo "#######################################################################################################"
 echo "#"
 echo "# ALL IN ONE SCRIPT THAT PERFORMS THE FOLLOWING TASKS:"
 echo "#"
-echo "# 0. LOGIN TO DOCKER HUB & PULL IMAGES"
+echo "# 0. DEALING WITH THE DOCKER HUB & THE RATE ON PULL IMAGES"
 echo "# 1. UPGRADE TO TRIDENT OPERATOR 20.10.1"
 echo "# 2. INSTALL FILE (NAS/RWX) BACKENDS FOR TRIDENT"
 echo "# 3. INSTALL BLOCK (iSCSI/RWO) BACKENDS FOR TRIDENT"
@@ -28,17 +20,64 @@ echo "#"
 echo "#######################################################################################################"
 echo
 
+if [ $(yum info jq | grep Repo | awk '{ print $3 }') != "installed" ]
+  then
+    echo "#######################################################################################################"
+    echo "Install JQ"
+    echo "#######################################################################################################"
+    yum install -y jq
+fi
+
 echo
 echo "#######################################################################################################"
 echo "#"
-echo "# 0. LOGIN TO DOCKER HUB & PULL IMAGES"
+echo "# 0. DEALING WITH THE DOCKER HUB & THE RATE ON PULL IMAGES"
 echo "#"
 echo "#######################################################################################################"
 echo
 
-sh Addendum/Addenda09/2_Lazy_Images/pull_all_images.sh rhel1 $1 $2
-sh Addendum/Addenda09/2_Lazy_Images/pull_all_images.sh rhel2 $1 $2
-sh Addendum/Addenda09/2_Lazy_Images/pull_all_images.sh rhel3 $1 $2
+TOKEN=$(curl "https://auth.docker.io/token?service=registry.docker.io&scope=repository:ratelimitpreview/test:pull" | jq -r .token)
+RATEREMAINING=$(curl --head -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest 2>&1 | grep  RateLimit-Remaining | cut -d ':' -f 2 | cut -d ';' -f 1 | cut -b 1- | tr -d ' ')
+
+if [ $RATEREMAINING -eq 0 ];then
+    echo "#"
+    echo "Your anonymous login to the Docker Hub does not have any pull left."
+    echo "Consider using your own credentials"
+    echo "#"
+    PULL=1
+elif [ $RATEREMAINING -lt 20 ];then
+    echo "#"
+    echo "Your anonymous login to the Docker Hub does not have many pull left ($RATEREMAINING)."
+    echo "Consider using your own credentials"
+    echo "#"
+    PULL=1
+else
+    echo "#"
+    echo "Your anonymous login to the Docker Hub seems to have plenty of pull left ($RATEREMAINING)."
+    echo "#"
+    PULL=0
+fi
+
+if [ $PULL -eq 1 ];then
+  if [ $# -eq 0 ];then
+    echo "No arguments supplied"
+    echo "Please restart the script with the following parameters:"
+    echo " - Parameter1: Docker hub login"
+    echo " - Parameter2: Docker hub password"
+    exit 0
+  else
+    echo
+    echo "#######################################################################################################"
+    echo "#"
+    echo "# 0. LOGIN TO DOCKER HUB & PULL IMAGES"
+    echo "#"
+    echo "#######################################################################################################"
+    echo
+
+    sh Addendum/Addenda09/2_Lazy_Images/pull_all_images.sh rhel1 $1 $2
+    sh Addendum/Addenda09/2_Lazy_Images/pull_all_images.sh rhel2 $1 $2
+    sh Addendum/Addenda09/2_Lazy_Images/pull_all_images.sh rhel3 $1 $2
+fi
 
 echo
 echo "#######################################################################################################"
