ebbdc2216fd7027908f662c9a42c4620d94caa80
Yvos <yves.weisser@gmail.com>
2021-04-27 16:17:30 +0200

    images mgmt update

--- Kubernetes_v4/Addendum/Addenda09/2_Lazy_Images/pull_all_images.sh
+++ Kubernetes_v4/Addendum/Addenda09/2_Lazy_Images/pull_all_images.sh
@@ -48,20 +48,17 @@ echo "# mysql:5.7"
 echo "####################################"
 ssh -o "StrictHostKeyChecking no" root@$1 docker pull mysql:5.7
 
-if [[ ! "$1" =~ ^(rhel1|rhel2|rhel3)$ ]]
-  then
-    echo "####################################"
-    echo "# netapp/trident:21.01.2"
-    echo "####################################"
-    ssh -o "StrictHostKeyChecking no" root@$1 docker pull netapp/trident:21.01.2
+echo "#############################################"
+echo "# Dealing with the Prometheus operator images"
+echo "#############################################"
 
-    echo "####################################"
-    echo "# netapp/trident-operator:21.01.2"
-    echo "####################################"
-    ssh -o "StrictHostKeyChecking no" root@$1 docker pull netapp/trident-operator:21.01.2
+GRAFANAHOST=$(kubectl get pod -n monitoring -l app.kubernetes.io/name=grafana -o=jsonpath='{.items[0].spec.nodeName}')
+if [[ $1 != $GRAFANAHOST ]];then
+  ssh -o "StrictHostKeyChecking no" root@$1 docker pull grafana/grafana:7.0.3
+  ssh -o "StrictHostKeyChecking no" root@$1 docker pull kiwigrid/k8s-sidecar:0.1.151
+fi
 
-    echo "####################################"
-    echo "# netapp/trident-autosupport:21.01"
-    echo "####################################"
-    ssh -o "StrictHostKeyChecking no" root@$1 docker pull netapp/trident-autosupport:21.01
+PROMETHEUSHOST=$(kubectl get -n monitoring pod -l app=prometheus-operator-operator -o=jsonpath='{.items[0].spec.nodeName}')
+if [[ $1 != $PROMETHEUSHOST ]];then
+  ssh -o "StrictHostKeyChecking no" root@$1 docker pull squareup/ghostunnel:v1.5.2
 fi
