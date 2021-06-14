766f6e999ccc495e8c6ad73d2df3fdf451d0eb68
Yvos <yves.weisser@gmail.com>
2021-06-14 14:57:32 +0200

    setup script update

--- Kubernetes_v4/Addendum/Addenda13/all_in_one.sh
+++ Kubernetes_v4/Addendum/Addenda13/all_in_one.sh
@@ -11,6 +11,7 @@ echo "#"
 echo "#######################################################################################################"
 echo
 
+cd ~/LabNetApp/Kubernetes_v4/Addendum/Addenda13
 sh addenda13_pull_images.sh $1 $2
 ssh -o "StrictHostKeyChecking no" root@rhel4 "svn export https://github.com/YvosOnTheHub/LabNetApp.git/trunk/Kubernetes_v4/Addendum/Addenda13"
 ssh -o "StrictHostKeyChecking no" root@rhel4 "cd Addenda13 && docker-compose up -d"
--- Kubernetes_v4/Scenarios/Scenario18/all_in_one.sh
+++ Kubernetes_v4/Scenarios/Scenario18/all_in_one.sh
@@ -10,24 +10,17 @@ echo " 2. Install Gitea on RHEL4"
 echo " 3. Install ArgoCD on Kubernetes"
 echo " 4. Push Scenario Docker Images to private repo"
 echo " 5. Uninstall Trident"
-echo " 6. Create a private repository on Gitea"
-echo " 7. Configuration local Git variables"
-echo " 8. Push code to Gitea"
+echo " 6. Configuration local Git variables"
 echo "#######################################################################################################"
 
-sh ../../Addenda/Addendum05/all_in_one.sh
-sh ../../Addenda/Addendum13/all_in_one.sh $1 $2
-sh ../../Addenda/Addendum14/all_in_one.sh
+sh ../../Addendum/Addenda05/all_in_one.sh
+sh ../../Addendum/Addenda13/all_in_one.sh $1 $2
+sh ../../Addendum/Addenda14/all_in_one.sh
+
+cd ~/LabNetApp/Kubernetes_v4/Scenarios/Scenario18
 sh push_scenario_images_to_private_repo.sh $1 $2
 sh trident_uninstall.sh
 
-
-echo "###### Create a repo"
-curl -X POST "http://192.168.0.64:3000/api/v1/user/repos" -u demo:netapp123 -H "accept: application/json" -H "content-type: application/json" -d '{
-  "name":"scenario18",
-  "description": "argocd repo"
-}'
-echo
 
 echo "###### Configure local Git variables"
 git config --global user.email lod.demo.netapp.com
@@ -37,11 +30,9 @@ git config --global alias.adcom '!git add -A && git commit -m'
 git config --global push.default simple
 echo
 
-echo "###### Puse code to Gitea"
-cp -R ~/LabNetApp/Kubernetes_v4/Scenarios/Scenario18/Repository ~/
-cd ~/Repository
-git init
-git add .
-git commit -m "initial commit"
-git remote add origin http://192.168.0.64:3000/demo/scenario18.git
-git push -u origin master
+echo "#######################################################################################################"
+echo " Connect to Gitea: http://192.168.0.64:3000/"
+echo " Create the administrator account: demo/netapp123/demo@demo.netapp.com"
+echo
+echo " Run all_in_one_post_process.sh to finish the setup"
+echo "#######################################################################################################"
