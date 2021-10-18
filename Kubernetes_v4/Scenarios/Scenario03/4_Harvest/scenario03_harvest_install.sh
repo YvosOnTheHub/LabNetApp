7dad5c2ec716258de0e41c89e1e78c082c80f9e1
Yvos <yves.weisser@gmail.com>
2021-10-18 11:43:43 +0200

    update with harvest 21.08

--- Kubernetes_v4/Scenarios/Scenario03/4_Harvest/scenario03_harvest_install.sh
+++ Kubernetes_v4/Scenarios/Scenario03/4_Harvest/scenario03_harvest_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ssh -o "StrictHostKeyChecking no" root@rhel6 wget https://github.com/NetApp/harvest/releases/download/v21.05.4/harvest-21.05.4-2.x86_64.rpm
+ssh -o "StrictHostKeyChecking no" root@rhel6 wget https://github.com/NetApp/harvest/releases/download/v21.08.0/harvest-21.08.0-6.x86_64.rpm
 sleep 15
 ssh -o "StrictHostKeyChecking no" root@rhel6 wget https://raw.githubusercontent.com/YvosOnTheHub/LabNetApp/master/Kubernetes_v4/Scenarios/Scenario03/4_Harvest/harvest.yml
-ssh -o "StrictHostKeyChecking no" root@rhel6 yum install -y harvest-21.05.4-2.x86_64.rpm
+ssh -o "StrictHostKeyChecking no" root@rhel6 yum install -y harvest-21.08.0-6.x86_64.rpm
 ssh -o "StrictHostKeyChecking no" root@rhel6 rm -f /opt/harvest/harvest.yml
 ssh -o "StrictHostKeyChecking no" root@rhel6 mv harvest.yml /opt/harvest/
 ssh -o "StrictHostKeyChecking no" root@rhel6 "cd /opt/harvest && bin/harvest start"
