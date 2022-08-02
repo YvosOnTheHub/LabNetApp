7ce87c068e0942a493229d882cd78c91576c8489
YvosMac <yves.weisser@netapp.com>
2022-08-02 11:01:06 +0200

    image mgmt for scenario01

--- Kubernetes_v5/Scenarios/Scenario01/1_Operator/all_in_one.sh
+++ Kubernetes_v5/Scenarios/Scenario01/1_Operator/all_in_one.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# OPTIONAL PARAMETERS:
 # - PARAMETER1: Docker hub login
 # - PARAMETER2: Docker hub password
 
-if [ $# -eq 2 ]
-  then
-    sh ../scenario01_pull_images.sh $1 $2
+if [[  $(docker images | grep registry | grep trident | grep 22.01.1 | wc -l) -eq 0 ]]; then
+  if [ $# -eq 0 ]; then
+      echo "No arguments supplied"
+      echo "Please add the following parameters to the shell script:"
+      echo " - Parameter1: Docker hub login"
+      echo " - Parameter2: Docker hub password"
+      exit 0
+  elif 
+       sh ../scenario01_pull_images.sh $1 $2
+  fi
 fi
 
 echo "#######################################################################################################"
