2b1b03014518261b35d5fd52292dc04b2971d761
YvosMac <yves.weisser@netapp.com>
2025-09-22 18:06:58 +0200

    removed archived chapters

--- Kubernetes_v4/Bonus/K8S_objects/script_add_affinity.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for file in $(ls pod_*.yaml) ; do
-  sed -i '/spec:/r script_add_affinity_text.txt' ${file}
-done
--- Kubernetes_v4/Bonus/K8S_objects/script_change_sc_in_pvc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for file in $(ls pvc*.yaml) ; do
-  sed -i 's/storage-class-nas/storage-class-nas-economy/' ${file}
-done
