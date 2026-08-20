2c9472fa6f504350ba15193d164e509ecc7a6544
YvosMac <yves.weisser@netapp.com>
2026-08-20 11:38:27 +0200

    typo

--- all_in_one_v6.sh
+++ all_in_one_v6.sh
@@ -658,7 +658,7 @@ fi
   echo
 
   TOKEN=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:ratelimitpreview/test:pull" | jq -r .token)
-  RATEREMAINING=$(curl --head -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest 2>&1 | grep -i ratelimit-remaining | cut -d ':' -f 2 | cut -d ';' -f 1 | cut -b 1- | tr -d ' ')
+  RATEREMAINING=$(curl --head -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest 2>&1 | grep -i '^ratelimit-remaining:' | cut -d ':' -f 2 | cut -d ';' -f 1 | xargs)
 
   echo "# Your anonymous login to the Docker Hub currently has $RATEREMAINING pulls left."
 }
